ce91f062f8094033694c43b57e78f6cb67ce6686
alejandro22UNA <e.morales.ale22@gmail.com>
2019-10-30 13:58:49 -0600

    Update cors.js

--- cors.js
+++ cors.js
@@ -7,9 +7,9 @@ function AllowCrossDomain() {
       ];
 
       var origen = req.headers.origin;
-      if (whiteList.indexOf(origen) >= -1) {
+     /* if (whiteList.indexOf(origen) >= -1) {
          res.setHeader('Access-Control-Allow-Origin', origen);
-      }
+      }*/
       res.header('Access-Control-Allow-Origin','*');
       res.header('Access-Control-Allow-Headers','Content-Type, Authorization');
       res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
