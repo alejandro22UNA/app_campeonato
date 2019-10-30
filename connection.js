d84a6fc8398d4344927d0cf81003eb02422ce561
alejandro22UNA <e.morales.ale22@gmail.com>
2019-10-29 22:03:58 -0600

    Update connection.js

--- connection.js
+++ connection.js
@@ -6,10 +6,10 @@ function Conexion(){
    this.inicia = function() {
       this.pool = mysql.createPool({
          connectionLimit: 10,
-         host: 'localhost',
-         user: 'root',
-         password: '123456789',
-         database: 'bd_campeonato'
+         host: '185.201.10.115',
+         user: 'u821432223_user00 ',
+         password: '4gxBJ7Kmi:j4JD1v',
+         database: 'u821432223_campeonato'
       })
    }
 
